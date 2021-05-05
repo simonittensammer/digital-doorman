f39038a235c0fe076582ddd0505abf229e0a6e07
simonittensammer <simon.ittensammer@gmx.at>
2021-05-05 23:05:53 +0200

    add telegram notification on endpoint call

--- doorman-backend/index.js
+++ doorman-backend/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 const port = 8000;
 
 const { exec } = require("child_process");
 
+const token = '1849138573:AAGru5zHNP1knBnev_BJ9SCrGUO6TDik2oY';
+const bot = new TelegramBot(token, { polling: true });
+let chatId = '';
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to Doorman!')
 });
 
 app.get('/openDoor', (req, res) => {
@@ -18,8 +23,9 @@ app.get('/openDoor', (req, res) => {
             // console.log('door close');
         });
     }, 1500);
-    
+
     console.log(Date.now() + ': Door opened');
+    bot.sendMessage(chatId, '🚪 Türe geöffnet!');
     res.send('Door opened');
 });
 
@@ -27,17 +33,24 @@ app.get('/openGarage', (req, res) => {
     exec("gpio -g write 16 0", (error, stdout, stderr) => {
         // console.log('garage start');
     });
-    
+
     const timeoutObj = setTimeout(() => {
         exec("gpio -g write 16 1", (error, stdout, stderr) => {
             // console.log('garage stop');
         });
     }, 250);
-    
+
     console.log(Date.now() + ': Garage opened');
+    bot.sendMessage(chatId, '🚘 Garage geöffnet!');
     res.send('Garage opened');
 });
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`)
+});
+
+bot.on('message', (msg) => {
+    chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Chat aktualisiert!');
 });
